bada16905749edcd096951ac443b37cf63ae38a4
FedericoCapitani <federico.capitani@outlook.it>
2022-05-04 10:16:22 +0200

    creare header menu dinamicamente

--- dc-comics/src/components/HeaderComponent.vue
+++ dc-comics/src/components/HeaderComponent.vue
@@ -5,16 +5,7 @@
                 <img src="@/assets/img/dc-logo.png" alt="Logo DC">
             </div>
             <div class="menu">
-                <p>CHARACTERS</p>
-                <p>COMICS</p>
-                <p>MOVIES</p>
-                <p>TV</p>
-                <p>GAMES</p>
-                <p>COLLECTIBLES</p>
-                <p>VIDEOS</p>
-                <p>FANS</p>
-                <p>NEWS</p>
-                <p>SHOP</p>
+                <a href="item.href" v-for="item in menu" :key="item.id"> {{item.text}} </a>
             </div>
         </nav>
     </div>
@@ -31,16 +22,18 @@
        align-items: center;
        column-gap: 1.5rem;
     }
-    .logo, .menu > p{
+    .logo, .menu > a{
         padding: 1rem 0;
+        text-decoration: none;
     }
-    .menu > p{
+    .menu > a{
         display: flex;
         align-items: center;
         font-weight: 700;
         height: 100%;
+        color: black;
     }
-    p:hover{
+    a:hover{
         color: #0282f9;
         border-bottom: 5px solid #0282f9;
         padding-top: calc(1rem + 5px);
@@ -48,7 +41,63 @@
 </style>
 
 <script>
-
-
-
+export default {
+    name: 'HeaderComponent',
+    data(){
+        return {
+            menu : [
+                {
+                    id: 1,
+                    href: '#',
+                    text: 'CHARACTERS'
+                },
+                {
+                    id: 2,
+                    href: '#',
+                    text: 'COMICS'
+                },
+                {
+                    id: 3,
+                    href: '#',
+                    text: 'MOVIES'
+                },
+                {
+                    id: 4,
+                    href: '#',
+                    text: 'TV'
+                },
+                {
+                    id: 5,
+                    href: '#',
+                    text: 'GAMES'
+                },
+                {
+                    id: 6,
+                    href: '#',
+                    text: 'COLLECTIBLES'
+                },
+                {
+                    id: 7,
+                    href: '#',
+                    text: 'VIDEOS'
+                },
+                {
+                    id: 8,
+                    href: '#',
+                    text: 'FANS'
+                },
+                {
+                    id: 9,
+                    href: '#',
+                    text: 'NEWS'
+                },
+                {
+                    id: 10,
+                    href: '#',
+                    text: 'SHOP'
+                }
+            ]
+        }
+    }
+};
 </script>
